04804ebdd6433d8e94b8d8d3446f427f52f531c6
SchokoMousse <kuehn.martin.j@gmail.com>
2010-05-21 11:12:24 +0200

    User signup complete

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # By using the symbol ':user', we get a Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                   "Martin Kuehn"
-  user.email                  "martin@kuehn.pro"
+  user.name                   "Example User"
+  user.email                  "example@railstutorial.org"
   user.password               "foobar"
   user.password_confirmation  "foobar"
 end
